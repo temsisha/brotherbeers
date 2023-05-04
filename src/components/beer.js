c38db7dc0f5a809c8899eea893790f6c54417c2f
temsisha <40714959+temsisha@users.noreply.github.com>
2023-05-04 13:56:35 +0200

    Update beer.js

--- src/components/beer.js
+++ src/components/beer.js
@@ -14,7 +14,8 @@ export default function Beer ({beer}){
         beerIBU,
         beerLowPrice,
         beerHighPrice,
-        newBeer,} = beer;
+        newBeer,
+	  soldBeer,} = beer;
     return (
         <div className="article-group">
             <div className="logo">
@@ -24,6 +25,7 @@ export default function Beer ({beer}){
             <div className="brand-visability">
                 <div className="brand-name">{beerName}</div>
                 <div className={newBeer ? "notify-badge" : ""}> {newBeer ? "NEW" : ""}</div>
+		<div className={soldBeer ? "notify-badge-sold" : ""}> {soldBeer ? "SOLD OUT" : ""}</div>
             </div>
                 <div className="brand-details">
                     <div className="brand-type">{beerType} <span className="separator">|</span> ABV <span className="type-detailed">{beerABV}</span> <span className="separator">|</span> IBU <span className="type-detailed">{beerIBU}</span></div>
